5431cc5e94ae8f8b1c203fdd1304eff2345c1339
Sergio <sergio@garciadelacruz.es>
2024-03-16 06:14:25 +0100

    41 current  2024-03-16 06:14:19  24.05pre596954.d691274a972b  6.6.21                          *

--- timeless.nix
+++ timeless.nix
@@ -82,6 +82,20 @@
   home-manager.users.sergio = {pkgs, ...}: {
     nixpkgs.config.allowUnfree = true;
     home.packages = [
+      (pkgs.writeTextFile {
+        name = "cursor-desktop-entry";
+        destination = "/share/applications/cursor.desktop";
+        text = ''
+          [Desktop Entry]
+          Version=1.0
+          Type=Application
+          Name=Cursor
+          Exec=/run/current-system/sw/bin/Cursor
+          Icon=cursor-icon
+          Terminal=false
+          Categories=Utility;
+        '';
+      })
     ];
     programs.bash.enable = true;
     programs.vscode = {
