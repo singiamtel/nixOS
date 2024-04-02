9c97dd45b314a0adbc14fed38787be95f9b5fee0
Sergio <sergio@garciadelacruz.es>
2024-04-02 12:39:04 +0200

    94 current  2024-04-02 12:38:57  24.05pre604424.d8fe5e6c92d0  6.6.23                          *

--- timeless.nix
+++ timeless.nix
@@ -90,61 +90,61 @@
       })
     ];
     # Enable zsh
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-      syntaxHighlighting.enable = true;
-      enableAutosuggestions = true;
-      oh-my-zsh = {
-        enable = true;
-        plugins = ["git" "sudo" "z"];
-        theme = "ys";
-      };
-      plugins = [
-        {
-          name = "fzf-tab";
-          src = pkgs.fetchFromGitHub {
-            owner = "Aloxaf";
-            repo = "fzf-tab";
-            rev = "bf3ef5588af6d3bf7cc60f2ad2c1c95bca216241";
-            hash = "sha256-0/YOL1/G2SWncbLNaclSYUz7VyfWu+OB8TYJYm4NYkM=";
-          };
-        }
-        {
-          name = "zsh-fzf-history-search";
-          src = pkgs.fetchFromGitHub {
-            owner = "joshskidmore";
-            repo = "zsh-fzf-history-search";
-            rev = "bf3ef5588af6d3bf7cc60f2ad2c1c95bca216241";
-            hash = "sha256-0/YOL1/G2SWncbLNaclSYUz7VyfWu+OB8TYJYm4NYkM=";
-          };
-        }
-        {
-          name = "zsh-autoswitch-virtualenv";
-          src = pkgs.fetchFromGitHub {
-            owner = "MichaelAquilina";
-            repo = "zsh-autoswitch-virtualenv";
-            rev = "4ddc42d3d84bfb36fac1eb48e9e6de33a92fa4f1";
-            hash = "sha256-hwg9wDMU2XqJ5FQEwMVVaz0n+xZ8NI82tH9VhLfFRC4=";
-          };
-        }
-      ];
-
-      initExtra = ''
-        export KEYTIMEOUT=1
-        bindkey -v
-        bindkey '^e' edit-command-line
-        bindkey "^?" backward-delete-char
-        bindkey -M viins '^R' history-incremental-pattern-search-backward
-        alias lg="lazygit"
-        alias e="nvim"
-        alias v="nvim"
-        alias -g G="|grep -Ei"
-        alias ls="eza"
-        alias cat="bat"
-        alias b="byobu-tmux"
-      '';
-    };
+    # programs.zsh = {
+    #   enable = true;
+    #   enableCompletion = true;
+    #   syntaxHighlighting.enable = true;
+    #   enableAutosuggestions = true;
+    #   oh-my-zsh = {
+    #     enable = true;
+    #     plugins = ["git" "sudo" "z"];
+    #     theme = "ys";
+    #   };
+    #   plugins = [
+    #     {
+    #       name = "fzf-tab";
+    #       src = pkgs.fetchFromGitHub {
+    #         owner = "Aloxaf";
+    #         repo = "fzf-tab";
+    #         rev = "bf3ef5588af6d3bf7cc60f2ad2c1c95bca216241";
+    #         hash = "sha256-0/YOL1/G2SWncbLNaclSYUz7VyfWu+OB8TYJYm4NYkM=";
+    #       };
+    #     }
+    #     {
+    #       name = "zsh-fzf-history-search";
+    #       src = pkgs.fetchFromGitHub {
+    #         owner = "joshskidmore";
+    #         repo = "zsh-fzf-history-search";
+    #         rev = "bf3ef5588af6d3bf7cc60f2ad2c1c95bca216241";
+    #         hash = "sha256-0/YOL1/G2SWncbLNaclSYUz7VyfWu+OB8TYJYm4NYkM=";
+    #       };
+    #     }
+    #     {
+    #       name = "zsh-autoswitch-virtualenv";
+    #       src = pkgs.fetchFromGitHub {
+    #         owner = "MichaelAquilina";
+    #         repo = "zsh-autoswitch-virtualenv";
+    #         rev = "4ddc42d3d84bfb36fac1eb48e9e6de33a92fa4f1";
+    #         hash = "sha256-hwg9wDMU2XqJ5FQEwMVVaz0n+xZ8NI82tH9VhLfFRC4=";
+    #       };
+    #     }
+    #   ];
+    #
+    #   initExtra = ''
+    #     export KEYTIMEOUT=1
+    #     bindkey -v
+    #     bindkey '^e' edit-command-line
+    #     bindkey "^?" backward-delete-char
+    #     bindkey -M viins '^R' history-incremental-pattern-search-backward
+    #     alias lg="lazygit"
+    #     alias e="nvim"
+    #     alias v="nvim"
+    #     alias -g G="|grep -Ei"
+    #     alias ls="eza"
+    #     alias cat="bat"
+    #     alias b="byobu-tmux"
+    #   '';
+    # };
 
     programs.neovim = {
       enable = true;
