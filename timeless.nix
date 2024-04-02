255986117699bb629170dd258b21092355ac8939
Sergio <sergio@garciadelacruz.es>
2024-04-02 14:18:26 +0200

    107 current  2024-04-02 14:18:18  24.05pre604424.d8fe5e6c92d0  6.8.2-zen2                          *

--- timeless.nix
+++ timeless.nix
@@ -168,7 +168,7 @@
     };
     programs.tmux = {
       enable = true;
-      shell = "${pkgs.zsh}/bin/zsh";
+      shell = "/run/current-system/sw/bin/zsh";
       terminal = "tmux-256color";
       historyLimit = 100000;
       plugins = with pkgs; [
