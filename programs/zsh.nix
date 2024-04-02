777a3203e685f45117e0f652110d6a6cdbf490b8
Sergio <sergio@garciadelacruz.es>
2024-04-02 19:59:30 +0200

    131 current  2024-04-02 19:59:21  24.05pre604424.d8fe5e6c92d0  6.8.2-zen2                          *

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -24,16 +24,17 @@
     };
   };
   environment.interactiveShellInit = ''
-    alias lg='lazygit'
-    alias -g G='| grep -Ei'
-    alias b='byobu-tmux'
-    alias e='nvim'
-    alias v='nvim'
-    alias r='ranger'
-    # bindkey -M viins '^R' fzf_history_search
-    bindkey -v
-      source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
-      source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+        alias lg='lazygit'
+        alias -g G='| grep -Ei'
+        alias b='byobu-tmux'
+        alias e='nvim'
+        alias v='nvim'
+        alias r='ranger'
+        # bindkey -M viins '^R' fzf_history_search
+        bindkey -v
+          source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+          source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+    KEYTIMEOUT=1
   '';
   users.defaultUserShell = pkgs.zsh;
 }
