01eecb434329df1748f849c30d2e483436016285
Sergio <sergio@garciadelacruz.es>
2024-04-02 12:50:29 +0200

    97 current  2024-04-02 12:50:21  24.05pre604424.d8fe5e6c92d0  6.8.2-zen2                          *

--- timeless.nix
+++ timeless.nix
@@ -9,6 +9,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
