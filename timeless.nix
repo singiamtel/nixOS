aff0624bb45852d727bca34e29043a06039a41d2
Sergio Garcia <sergio@garciadelacruz.es>
2024-04-05 01:01:40 +0200

    151 current  2024-04-04 22:01:20  24.05pre606852.fd281bd6b7d3  6.6.24                              *

--- timeless.nix
+++ timeless.nix
@@ -11,7 +11,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  #boot.kernelPackages = pkgs.linuxPackages_zen;
 
   imports = [
     ./systempkgs.nix
