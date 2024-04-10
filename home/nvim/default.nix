b2ad24320d8e7c03724f5a638b3d624afd1d45fb
Sergio Garcia <sergio@garciadelacruz.es>
2024-04-10 07:59:12 +0200

    175 current  2024-04-10 07:59:03  23.11.20240408.d272ca5       6.1.84                              *

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -42,6 +42,7 @@
           tsserver.enable = true;
           lua-ls.enable = true;
           pylsp.enable = true;
+          nil_ls.enable = true;
           rust-analyzer = {
             enable = true;
             installCargo = true;
