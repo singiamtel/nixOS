58f7d87c164ada6203edca7057028bae66423e74
Sergio Garcia <sergio@garciadelacruz.es>
2024-05-13 03:50:09 +0200

    235 current  2024-05-13 03:50:06  24.05.20240412.cfd6b5f  6.6.26                          *

--- machines/timeless/timeless.nix
+++ machines/timeless/timeless.nix
@@ -130,7 +130,7 @@
     pkgs.jellyfin-ffmpeg
   ];
 
-  networking.firewall.allowedTCPPorts = [22 3000 8000 80 443 25565];
+  networking.firewall.allowedTCPPorts = [22 3000 8000 80 443 25565 8096];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
